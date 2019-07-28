d0d40f96a79f0a6ef7762aeba4c4f96787e712c3
Lev Ozeryansky <lev.ozeryansky@ericom.com>
2019-07-28 12:58:12 +0300

    Centos script tested and working

--- install-docker-centos.sh
+++ install-docker-centos.sh
@@ -1,8 +1,26 @@
 #!/bin/bash -x
 
-yum install -y yum-utils device-mapper-persistent-data lvm2
+DOCKER_VERSION="18.09.7"
+
+if [ -n "$1" ]; then
+    DOCKER_VERSION="$1"
+fi
+
+systemctl stop firewalld 
+systemctl disable firewalld
+
+
+yum install -y yum-utils device-mapper-persistent-data lvm
+
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-yum install -y docker-ce
+DOCKER_INSTALL_VERSION=$(repoquery --show-duplicates docker-ce | grep $DOCKER_VERSION)
 
+yum install -y $DOCKER_INSTALL_VERSION
+
+systemctl enable docker.service
 systemctl start docker
+
+DOCKER_CLI_VERSION=$(repoquery --show-duplicates docker-ce-cli* | grep $DOCKER_VERSION)
+
+yum downgrade -y $DOCKER_CLI_VERSION
